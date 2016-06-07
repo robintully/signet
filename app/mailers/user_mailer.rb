2cd4ab45707ea8d655acc598efc1ef09283b53a0
wlschnee <will@willschneekloth.com>
2016-06-07 09:57:44 -0400

    initial actionmailer setup

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+  default from: "signet.applicaiton@gmail.com"
+
+  def registration_confirmation(user)
+    @user = user
+    email_with_name = %("#{user.name}" <#{user.email_address}>")
+    mail(to: email_with_name, subject: "Registration confirmation for Signet")
+  end
+
+end
