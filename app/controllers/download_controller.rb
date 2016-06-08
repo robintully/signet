76d657215cd2ef824da1dfcaa65bd32bd9f88c8a
robintully <mr.robintully@gmail.com>
2016-06-08 14:56:15 -0400

    fixed single downlaod to send as data with correct file name

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -1,8 +1,10 @@
 class DownloadController < ApplicationController
-    def download_single
-    zipped_file = FileZipper.perform(params[:parchment_id])
-    send_file zipped_file.archive_file, disposition: 'attachment'
-  end
+  def download_single
+  url = (Parchment.find_by_id(params[:parchment_id])).file.url
+  data = open(url)
+  file_name = Parchment.find_by_id(params[:parchment_id])[:file]
+  send_data data.read, filename: file_name
+end
 
 end
 
